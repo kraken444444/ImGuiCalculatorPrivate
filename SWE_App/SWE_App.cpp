105ef849dc6ebb091480175036a3bc70b96009e8
kraken444444 <166565143+kraken444444@users.noreply.github.com>
2024-11-08 13:50:44 -0500

    0.1.0 Added Main, and initialization code

--- SWE_App/SWE_App.cpp
+++ SWE_App/SWE_App.cpp
@@ -1,19 +1,104 @@
-// SWE_App.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_sdlrenderer2.h"
+#include <stdio.h>
+#include <SDL.h>
+#include <string>
+#include <windows.h>
+#include <SDL_config_windows.h>
+#include <SDL_syswm.h>
 
-#include <iostream>
 
-int main()
+int main(int, char**)
 {
-	// wxWidgets library should be installed and compiled at C:\wxWidgets
-	// Open properties for Visual Studio project
-	// Select C/C++ -> General -> Addition Include Directories
-	// C:\wxWidgets\include;C:\wxWidgets\include\msvc
-	// Select Linker -> Additional Library Dependencies
-	// C:\wxWidgets\lib\vc_lib; (If building for 64-bit, use 64-bit libraries throughout the settings e.g. C:\wxWidgets\lib\vc_x64_lib )
-	// Select Linker -> System -> Subsystem
-	// Change from Console to Windows
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER);
 
-	// Once all steps above have been completed, delete this .cpp file
-	// and create your App class
+
+    SDL_DisplayMode displayMode;
+    SDL_GetCurrentDisplayMode(0, &displayMode);
+
+    int screenWidth = displayMode.w;
+    int screenHeight = displayMode.h;
+
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+SDL_Renderer example for SWE class", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_BORDERLESS | SDL_WINDOW_POPUP_MENU); // |SDL_ALWAYSONTOP
+
+    SDL_SysWMinfo wmInfo; //variable that stores window manager information
+    SDL_VERSION(&wmInfo.version); //initializes current version of info so we can pass it to getwindowinfo
+    SDL_GetWindowWMInfo(window, &wmInfo);//getter for window info and stores it inside wmInfo
+    HWND hwnd = wmInfo.info.win.window; // extracts info from wmInfo and stores it inside HWND
+
+    SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_EXSTYLE) | WS_EX_LAYERED); //Sets window style, allowing it to be transparent
+    SetLayeredWindowAttributes(hwnd, 0, 0, LWA_COLORKEY); // sets the window info of hwnd, LWA_COLORKEY sets it to transparent 0, 0, 0 color key is black, so any pixels matching will be transparent
+
+
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+    ImGui::StyleColorsClassic();
+
+
+
+    ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer2_Init(renderer);
+
+
+    bool show_demo_window = false;
+    bool show_another_window = false;
+
+    bool done = false;
+    while (!done)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT)
+                done = true;
+            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
+                done = true;
+        }
+        if (SDL_GetWindowFlags(window) & SDL_WINDOW_MINIMIZED)
+        {
+            SDL_Delay(10);
+            continue;
+        }
+
+    
+
+        ImGui_ImplSDLRenderer2_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+       
+
+
+
+        ImGui::Render();
+        SDL_RenderSetScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        // SDL_SetRenderDrawColor(renderer, (Uint8)(clear_color.x * 255), (Uint8)(clear_color.y * 255), (Uint8)(clear_color.z * 255), (Uint8)(clear_color.w * 255));
+        SDL_RenderClear(renderer);
+        ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData(), renderer);
+        SDL_RenderPresent(renderer);
+
+
+
+    }
+
+
+    ImGui_ImplSDLRenderer2_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
+
